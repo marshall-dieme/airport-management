2e4fb6a9e69a1991177df72decbeafb7b6cbb5f9
Nene-DIAKHATE <108729271+Nene-DIAKHATE@users.noreply.github.com>
2022-09-19 20:59:45 +0000

    Add files via upload

---
+++ naming-server-airplane/src/test/java/com/microservices/airplane/namingserverairplane/NamingServerAirplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.airplane.namingserverairplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerAirplaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
