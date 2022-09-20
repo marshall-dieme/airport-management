32a98ace2678f5dd59fc8f1a8a2ec96626bff87e
Ndeye Nafy Mbengue <nafnaa14@gmail.com>
2022-09-20 13:53:23 +0000

    add AirportconfigServer

---
+++ Airport-Configserver/src/test/java/com/microservices/airportconfigserver/AirportConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.airportconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
