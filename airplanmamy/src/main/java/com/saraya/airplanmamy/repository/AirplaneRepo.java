21af47032b0a7e9f6b4f9d7868e205d3d0e71914
bineta24 <62804165+bineta24@users.noreply.github.com>
2022-09-22 18:35:30 +0000

    Delete airplanmamy directory

--- airplanmamy/src/main/java/com/saraya/airplanmamy/repository/AirplaneRepo.java
+++
@@ -1,8 +0,0 @@
-package com.saraya.airplanmamy.repository;
-
-import com.saraya.airplanmamy.bean.Airplane;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AirplaneRepo extends JpaRepository<Airplane,Integer> {
-    Airplane findByAirplaneId(int id);
-}
