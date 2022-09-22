ce87b91ce0088e6a21b0bb3fd4f9f9f912ee713d
ndeye gaye <gayeeendeyeee@gmail.com>
2022-09-22 21:10:07 +0000

    adding passager files

---
+++ passager-service/src/main/java/com/saraya/passagerservice/PassagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saraya.passagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassagerServiceApplication.class, args);
+	}
+
+}
---
+++ passager-service/src/test/java/com/saraya/passagerservice/PassagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraya.passagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
