b846f114870c86ca1e7c5034843b5737e77b2734
bineta24 <62804165+bineta24@users.noreply.github.com>
2022-09-22 15:28:37 +0000

    Add files via upload

---
+++ airplanmamy/src/main/java/com/saraya/airplanmamy/AirplanmamyApplication.java
@@ -0,0 +1,13 @@
+package com.saraya.airplanmamy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplanmamyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplanmamyApplication.class, args);
+	}
+
+}
