ee3f821e13d884cf7616393f83dbcc400dd73df6
Fatoumata221 <109517841+Fatoumata221@users.noreply.github.com>
2022-09-22 10:06:19 +0100

    Add files via upload

---
+++ Employee/src/main/java/com/example/employee/bean/Employee.java
@@ -0,0 +1,158 @@
+package com.example.employee.bean;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+@Entity
+@Table(name = "Employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int EmployeeId;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthday;
+    private String sex;
+    private String street;
+    private String city;
+    private String zip;
+    private String country;
+    private String email;
+    private String telephone;
+    private double salary;
+    @Column(unique = true)
+    private String username;
+    private String Password;
+
+    public Employee(int employeeId, String firstName, String lastName, LocalDate birthday, String sex, String street, String city, String zip, String country, String email, String telephone, double salary, String username, String password) {
+        this.EmployeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.sex = sex;
+        this.street = street;
+        this.city = city;
+        this.zip = zip;
+        this.country = country;
+        this.email = email;
+        this.telephone = telephone;
+        this.salary = salary;
+        this.username = username;
+        this.Password = password;
+    }
+
+    public Employee() {
+    }
+
+    public int getEmployeeId() {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        EmployeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
