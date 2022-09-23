fa73edef1a8d0aff4e262d2f8f6e915d842e6578
Ya Moustapha <101671327+Yamoustapha@users.noreply.github.com>
2022-09-23 01:40:29 +0000

    added airline

---
+++ airline/src/main/java/com/saraya/airline/model/Airline.java
@@ -0,0 +1,19 @@
+package com.saraya.airline.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Airline {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "airline_id", nullable = false)
+    private Long airline_id;
+
+    private String airline_name;
+
+    private String iata;
+
+    public Long getAirline_id() {
+        return airline_id;
+    }
+}
---
+++ airline/src/test/java/com/saraya/airline/AirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraya.airline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
