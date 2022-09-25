b7319508fe11a9bf1ccbf0e0dbb74742f4047fa6
OusmanGueye <104196252+OusmanGueye@users.noreply.github.com>
2022-09-25 21:27:55 +0000

    Add files via upload

---
+++ time-table-service/src/main/java/com/saraya/timetableservice/TimeTableServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saraya.timetableservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTableServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeTableServiceApplication.class, args);
+    }
+
+}
