a893740b7678a4e9f1843b1760171df959141611
Fatoumata Badiane <109517841+Fatoumata221@users.noreply.github.com>
2022-09-25 20:43:37 +0100

    Delete Airport-service directory

--- Airport-service/src/main/java/com/saraya/airportservice/repo/AirportRepo.java
+++
@@ -1,9 +0,0 @@
-package com.saraya.airportservice.repo;
-
-import com.saraya.airportservice.bean.Airport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AirportRepo extends JpaRepository<Airport,Integer> {
-    Airport findByAirportId(int id);
-
-}
