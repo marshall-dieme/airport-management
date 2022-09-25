5d7cf3c5ff6baa071e3358459e3af405f3c6f4df
OusmanGueye <104196252+OusmanGueye@users.noreply.github.com>
2022-09-25 11:25:41 +0000

    Add files via upload

---
+++ parking-service/src/main/java/com/saraya/parkingservice/ParkingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saraya.parkingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingServiceApplication.class, args);
+    }
+
+}
