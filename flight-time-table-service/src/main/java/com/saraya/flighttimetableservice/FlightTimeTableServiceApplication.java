38c701b4c444dd32f5f851500399b968a2e1874d
OusmanGueye <104196252+OusmanGueye@users.noreply.github.com>
2022-09-25 21:28:15 +0000

    Add files via upload

---
+++ flight-time-table-service/src/main/java/com/saraya/flighttimetableservice/FlightTimeTableServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saraya.flighttimetableservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTimeTableServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightTimeTableServiceApplication.class, args);
+    }
+
+}
