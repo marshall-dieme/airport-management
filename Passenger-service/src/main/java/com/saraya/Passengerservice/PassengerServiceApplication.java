7e04f419bf1f941ea9c610c8c7a18fd8b5a9d7f2
Diallocodeur <diallocodeur@gmail.com>
2022-09-27 12:36:27 +0000

    update and reformat code

--- Passenger-service/src/main/java/com/saraya/Passengerservice/PassengerServiceApplication.java
+++ Passenger-service/src/main/java/com/saraya/Passengerservice/PassengerServiceApplication.java
@@ -2,8 +2,10 @@ package com.saraya.Passengerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PassengerServiceApplication {
 
 	public static void main(String[] args) {
