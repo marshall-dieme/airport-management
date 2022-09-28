aeb7226ae3808199e5da919a374d959ea4f14fe3
Cheikh Sow <102799486+kheucha12@users.noreply.github.com>
2022-09-28 17:04:25 +0100

    Add files via upload

--- Airport/src/main/java/com/example/airp/AirportApplication.java
+++ Airport/src/main/java/com/example/airp/AirportApplication.java
@@ -2,7 +2,9 @@ package com.example.airp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+@EnableFeignClients
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.example.airp.model3")
