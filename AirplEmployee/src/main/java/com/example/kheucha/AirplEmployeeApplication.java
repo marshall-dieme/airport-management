0d37d6258984328966fea5e5b9d9eb74751a0cf0
Cheikh Sow <102799486+kheucha12@users.noreply.github.com>
2022-09-28 18:12:22 +0100

    Add files via upload

--- AirplEmployee/src/main/java/com/example/kheucha/AirplEmployeeApplication.java
+++ AirplEmployee/src/main/java/com/example/kheucha/AirplEmployeeApplication.java
@@ -2,9 +2,12 @@ package com.example.kheucha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableFeignClients
+
 @ComponentScan(basePackages="com.example.kheucha.model")
 public class AirplEmployeeApplication {
 
