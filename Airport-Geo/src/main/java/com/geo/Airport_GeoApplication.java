0d4991ab04af2f476394f855223e7ee69004bf01
Cheikh Sow <102799486+kheucha12@users.noreply.github.com>
2022-09-28 17:01:52 +0100

    Add files via upload

--- Airport-Geo/src/main/java/com/geo/Airport_GeoApplication.java
+++ Airport-Geo/src/main/java/com/geo/Airport_GeoApplication.java
@@ -2,9 +2,12 @@ package com.geo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableFeignClients
+
 @ComponentScan(basePackages="com.geo.model4")
 
 public class Airport_GeoApplication {
