b443ad552e7764a9669f627b7e33f184002c45c8
Cheikh Sow <102799486+kheucha12@users.noreply.github.com>
2022-09-29 02:08:20 +0100

    Add files via upload

--- Airline/src/main/java/com/saraya/AirlineApplication.java
+++ Airline/src/main/java/com/saraya/AirlineApplication.java
@@ -2,9 +2,10 @@ package com.saraya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
-
+@EnableEurekaClient
 @SpringBootApplication
 public class AirlineApplication {
 
