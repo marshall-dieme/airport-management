0f5d0bc31acc4d6deb564ef01f6574133368154a
Regulus <alassaneseck94@gmail.com>
2022-09-29 10:44:04 +0000

    Making the relation between employee-service and service-service

---
+++ empServiceRelation/src/test/java/com/regulus/empservicerelation/EmpServiceRelationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regulus.empservicerelation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpServiceRelationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
