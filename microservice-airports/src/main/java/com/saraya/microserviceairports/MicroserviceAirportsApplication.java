8add7d8ad3c941d0aeacfede55c9930333e9794e
Ibrahim Ahamada <ibrahimahamada001a@gmail.com>
2022-10-03 13:53:56 +0000

    start to use restTemplete

--- microservice-airports/src/main/java/com/saraya/microserviceairports/MicroserviceAirportsApplication.java
+++ microservice-airports/src/main/java/com/saraya/microserviceairports/MicroserviceAirportsApplication.java
@@ -5,10 +5,11 @@ import com.saraya.microserviceairports.repositories.AirportRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class MicroserviceAirportsApplication implements CommandLineRunner {
     private final AirportRepository airportRepository;
 
